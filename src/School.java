e1da6dbbdfba31f8c39325ea51565b6f768fa242
kachmura <chmuraka1998@gmail.com>
2022-10-09 14:51:10 +0200

    new feature

--- src/School.java
+++ src/School.java
@@ -1,7 +1,7 @@
 public class School {
     public static void main(String[] args) {
         Student student = new Student("Jan","Kowalski" , 'm', "Java Web Developer", 1 );
-        student.info(student);
+        student.info(student); //to jest wywołanie informacji
 
     }
 }
